4c2d1c9dbe10e6d9fdc52b703b19c6d50142c619
Sebastian <sebastian97@gmail.com>
2024-07-14 15:35:01 -0700

    Cleaner text and improved mixin

--- src/main/java/me/sebastian420/PandaDeathBan/BanMessageUtil.java
+++ src/main/java/me/sebastian420/PandaDeathBan/BanMessageUtil.java
@@ -18,14 +18,30 @@ public class BanMessageUtil {
         long minutes = remainingTime / 60;
 
 
-        MutableText message = MutableText.of(Text.of("").getContent()).append(Text.of("You are Death Banned").copy().formatted(Formatting.RED))
+        MutableText message = MutableText.of(Text.of("").getContent()).append(Text.of("☠ You are Dead ☠").copy().formatted(Formatting.RED))
                 .append("\n")
-                .append(Text.of("You will be unbanned in: ").copy().formatted(Formatting.WHITE));
+                .append("\n")
+                .append(Text.of("You can join in: ").copy().formatted(Formatting.WHITE));
 
-        if (days > 0) message.append(Text.of(String.format("%d days ", days)).copy().formatted(Formatting.YELLOW));
-        if (hours > 0) message.append(Text.of(String.format("%d hours ", hours)).copy().formatted(Formatting.YELLOW));
-        message.append(Text.of(String.format("%d minutes", minutes)).copy().formatted(Formatting.YELLOW));
-
+        if (days > 0) {
+            message.append(formatTimeUnit(days, "day")).formatted(Formatting.YELLOW);
+        }
+        if (hours > 0) {
+            if (days > 0) message.append(Text.of(" "));
+            message.append(formatTimeUnit(hours, "hour")).formatted(Formatting.YELLOW);
+        }
+        if (minutes > 0) {
+            if (days > 0 || hours > 0) message.append(Text.of(" "));
+            message.append(formatTimeUnit(minutes, "minute")).formatted(Formatting.YELLOW);
+        }
         return message;
+    }
+
+    private static MutableText formatTimeUnit(long value, String unit) {
+        MutableText text = MutableText.of(Text.of(String.format("%d %s", value, unit)).getContent());
+        if (value != 1) {
+            text.append("s");
+        }
+        return text;
     }
 }
--- src/main/java/me/sebastian420/PandaDeathBan/mixin/KickOnJoinMixin.java
+++ src/main/java/me/sebastian420/PandaDeathBan/mixin/KickOnJoinMixin.java
@@ -1,29 +1,30 @@
 package me.sebastian420.PandaDeathBan.mixin;
 
+import com.mojang.authlib.GameProfile;
 import me.sebastian420.PandaDeathBan.StateSaverAndLoader;
-import net.minecraft.network.ClientConnection;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ConnectedClientData;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.net.SocketAddress;
 
 import static me.sebastian420.PandaDeathBan.BanMessageUtil.createBanMessage;
 
 @Mixin(PlayerManager.class)
-public class KickOnJoinMixin {
-    @Inject(at = @At("HEAD"), method = "onPlayerConnect", cancellable = true)
-    private void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo ci) {
-        StateSaverAndLoader.PlayerDeathBanData playerData = StateSaverAndLoader.getPlayerState(player);
+public abstract class KickOnJoinMixin {
+    @Shadow public abstract MinecraftServer getServer();
 
+    @Inject(at = @At("TAIL"), method = "checkCanJoin", cancellable = true)
+    public void checkCanJoin(SocketAddress address, GameProfile profile, CallbackInfoReturnable<Text> cir) {
+        StateSaverAndLoader.PlayerDeathBanData playerData = StateSaverAndLoader.getPlayerState(profile.getId(), this.getServer());
         long currentTimeMillis = System.currentTimeMillis() / 1000L;
-
         if (playerData.deathUnbanTime > currentTimeMillis) {
-            connection.disconnect(createBanMessage(playerData.deathUnbanTime));
+            cir.setReturnValue(createBanMessage(playerData.deathUnbanTime));
         }
-
-        ci.cancel();
     }
 }
