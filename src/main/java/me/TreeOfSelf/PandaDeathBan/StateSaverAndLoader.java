833dcbe4a814e5e40dd919a8a0325ad37793159a
Sebastian <sebastian97@gmail.com>
2025-04-30 16:35:00 -0700

    1.21.5 update

--- src/main/java/me/TreeOfSelf/PandaDeathBan/StateSaverAndLoader.java
+++ src/main/java/me/TreeOfSelf/PandaDeathBan/StateSaverAndLoader.java
@@ -1,28 +1,51 @@
 package me.TreeOfSelf.PandaDeathBan;
 
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.*;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.PersistentState;
 import net.minecraft.world.PersistentStateManager;
+import net.minecraft.world.PersistentStateType;
 import net.minecraft.world.World;
 
 import java.util.HashMap;
 import java.util.UUID;
 
+import static me.TreeOfSelf.PandaDeathBan.PandaDeathBan.MOD_ID;
+
 public class StateSaverAndLoader extends PersistentState {
 
-    public HashMap<UUID, PlayerDeathBanData> players = new HashMap<>();
+    public static Codec<StateSaverAndLoader> codec(ServerWorld world) {
+        return Codec.of(new Encoder<>() {
+            @Override
+            public <T> DataResult<T> encode(StateSaverAndLoader stateSaverAndLoader, DynamicOps<T> dynamicOps, T t) {
+                NbtCompound nbtCompound = new NbtCompound();
+                stateSaverAndLoader.writeNbt(nbtCompound);
+                return DataResult.success((T) nbtCompound);
+            }
+        }, new Decoder<>() {
+            @Override
+            public <T> DataResult<Pair<StateSaverAndLoader, T>> decode(DynamicOps<T> ops, T input) {
+                NbtCompound nbtCompound = (NbtCompound) ops.convertTo(NbtOps.INSTANCE, input);
+                StateSaverAndLoader partnerState = createFromNbt(nbtCompound, world.getRegistryManager());
+                return DataResult.success(Pair.of(partnerState, ops.empty()));
+            }
+        });
+    }
 
     public static StateSaverAndLoader createFromNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
         StateSaverAndLoader state = new StateSaverAndLoader();
 
-        NbtCompound playersNbt = tag.getCompound("players");
+        NbtCompound playersNbt = tag.getCompound("players").get();
         playersNbt.getKeys().forEach(key -> {
             PlayerDeathBanData playerData = new PlayerDeathBanData();
 
-            playerData.deathUnbanTime = playersNbt.getCompound(key).getLong("deathUnbanTime");
+            playerData.deathUnbanTime = playersNbt.getCompound(key).get().getLong("deathUnbanTime").get();
 
             UUID uuid = UUID.fromString(key);
             state.players.put(uuid, playerData);
@@ -31,15 +54,38 @@ public class StateSaverAndLoader extends PersistentState {
         return state;
     }
 
-    private static final Type<StateSaverAndLoader> type = new Type<>(
-            StateSaverAndLoader::new,
-            StateSaverAndLoader::createFromNbt,
-            null
-    );
+    public HashMap<UUID, PlayerDeathBanData> players = new HashMap<>();
+
+    public static StateSaverAndLoader readNbt(NbtCompound tag) {
+        StateSaverAndLoader state = new StateSaverAndLoader();
+
+        NbtCompound playersNbt = tag.getCompound("players").get();
+        playersNbt.getKeys().forEach(key -> {
+            PlayerDeathBanData playerData = new PlayerDeathBanData();
+
+            playerData.deathUnbanTime = playersNbt.getCompound(key).get().getLong("deathUnbanTime").get();
+
+            UUID uuid = UUID.fromString(key);
+            state.players.put(uuid, playerData);
+        });
+
+        return state;
+    }
+
+
+
 
     public static StateSaverAndLoader getServerState(MinecraftServer server) {
         PersistentStateManager persistentStateManager = server.getWorld(World.OVERWORLD).getPersistentStateManager();
-        StateSaverAndLoader state = persistentStateManager.getOrCreate(type, PandaDeathBan.MOD_ID);
+
+        PersistentStateType<StateSaverAndLoader> type = new PersistentStateType<>(
+                MOD_ID,
+                StateSaverAndLoader::new,
+                codec(server.getWorld(World.OVERWORLD)),
+                null
+        );
+
+        StateSaverAndLoader state = persistentStateManager.getOrCreate(type);
         state.markDirty();
         return state;
     }
@@ -54,8 +100,7 @@ public class StateSaverAndLoader extends PersistentState {
         return serverState.players.computeIfAbsent(playerUUID, uuid -> new PlayerDeathBanData());
     }
 
-    @Override
-    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+    public NbtCompound writeNbt(NbtCompound nbt) {
         NbtCompound playersNbt = new NbtCompound();
         players.forEach((uuid, playerData) -> {
             NbtCompound playerNbt = new NbtCompound();
